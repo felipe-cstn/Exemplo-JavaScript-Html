416ad48579aa0cae4cc59777ab2a3b6c42e49543
Felipe Celestino <84358544+felipe-cstn@users.noreply.github.com>
2022-10-26 19:55:43 -0300

    Add files via upload

---
+++ array_dicio/js/main.js
@@ -0,0 +1,38 @@
+var data = new Date();
+console.log(data);
+//console.log(data.getSeconds());
+//var sonhos = ["casa", "carro", "salario"];
+//console.log (sonhos);
+//var i = 0;
+//var idade = prompt("Qual sua idade? "); 
+
+/*
+console.log("---VETOR---");
+    for (; i<3;i++){
+        console.log("posicao: ",i);
+        console.log(sonhos[i]);
+    }*/ 
+
+/*
+    var idade = prompt("Qual sua idade? ");
+function apto(id){
+    
+    if (id >= 59){
+            return alert("APTO A RECEBER A VACINA!" );
+    } else{
+            return alert ("AGUARDE SUA VEZ, FELA DA PUTA!");
+    }
+}
+*/
+
+function clique(){
+    //alert("Vai clicar na casa do carai, só quem pode clicar é nois!");
+    document.getElementById("agradecimento").innerHTML = "Virus instalado com sucesso, obrigado pela colaboração! "
+    
+}
+function passeMouse(){
+    document.getElementById("mouse").innerHTML = "Tira o mouse daí, vagabundo"
+}
+
+
+//apto(idade);
